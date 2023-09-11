2a4250b7b49fc2d4916007dde3cba2e720eaaec0
Thanh Le <thanhledev@gmai.com>
2023-09-11 10:09:14 +0200

    learn-kotlin-program: OOP init blocks

--- learn-kotlin-program/src/main/kotlin/Car.kt
+++ learn-kotlin-program/src/main/kotlin/Car.kt
@@ -1,4 +1,15 @@
-class Car(var name: String, var model: String, var color: String, var doors: Int) {
+class Car(name: String, var model: String, var color: String, var doors: Int) {
+    lateinit var name: String
+
+    init {
+        if (name.lowercase().startsWith("a")) {
+            this.name = name
+        } else {
+            this.name = "User"
+            println("The user does not start with the 'a' or 'A' letter.")
+        }
+    }
+
     fun move() {
         println("The car $name is moving now...")
     }
