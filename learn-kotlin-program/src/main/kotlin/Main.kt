998846fa7729ad4791e1b27c0859d9ba6a235b2c
Thanh Le <thanhle@legion5.localdomain>
2023-09-28 16:31:42 +0200

    learning-kotlin: Zipping & Pair

--- learn-kotlin-program/src/main/kotlin/Main.kt
+++ learn-kotlin-program/src/main/kotlin/Main.kt
@@ -440,12 +440,25 @@ fun main(args: Array<String>) {
     users.forEach { i, s -> println("$i -> $s") }*/
 
     // Mapping
-    val numbers = setOf(1,2,3,4,5)
+    /*val numbers = setOf(1,2,3,4,5)
     println(numbers.map { if (it == 2) it * 100 else it * 10 })
 
     val numbersMap = mapOf("key 1" to 1, "key 2" to 2, "key 3" to 3, "key 4" to 4)
     println(numbersMap.mapKeys { it.key.uppercase(Locale.getDefault()) })
-    println(numbersMap.mapValues { it.value + it.key.length })
+    println(numbersMap.mapValues { it.value + it.key.length })*/
+
+    // Zipping
+    val colors = listOf("red", "brown", "gray")
+    val animals = listOf("fox", "bear", "wolf")
+    // => create pairs
+    println(colors.zip(animals))
+
+    // Pair => https://www.geeksforgeeks.org/pair-in-kotlin/
+    val (x,y) = Pair(1, "Geeks")
+    println(x)
+    println(y)
+
+
 }
 
 fun getSealedClassData(result: Result) {
